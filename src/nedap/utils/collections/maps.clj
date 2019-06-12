7e85a2e9f6a770f43b62a603ebe0b9f5f2bfb295
vemv <vemv@users.noreply.github.com>
2019-06-12 11:36:49 +0200

    Implement `nedap.utils.collections.maps/invert`
    Fixes nedap/utils.collections#2

---
+++ src/nedap/utils/collections/maps.clj
@@ -0,0 +1,17 @@
+(ns nedap.utils.collections.maps
+  (:require
+   [clojure.set :as set]
+   [nedap.utils.spec.api :refer [check!]]
+   [nedap.utils.speced :as speced]))
+
+(speced/defn ^boolean? unambigiously-invertable?
+  [^map? m]
+  (let [vs (vals m)]
+    (or (empty? vs)
+        (apply distinct? vs))))
+
+(speced/defn ^map? invert
+  "Returns the map with the vals mapped to the keys."
+  [^map? m]
+  {:pre [(check! unambigiously-invertable? m)]}
+  (set/map-invert m))
