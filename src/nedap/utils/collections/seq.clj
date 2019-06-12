349778f4d0bda5432800e1de47cf262bfae11277
vemv <vemv@users.noreply.github.com>
2019-06-12 08:52:52 +0200

    Avoid `check!`

--- src/nedap/utils/collections/seq.clj
+++ src/nedap/utils/collections/seq.clj
@@ -3,7 +3,6 @@
 
   Functions contained here should be lazy, else they should live in the `eager` ns."
   (:require
-   [nedap.utils.spec.api :refer [check!]]
    [nedap.utils.spec.predicates :refer :all]
    [nedap.utils.speced :as speced]))
 
@@ -11,15 +10,12 @@
   "Sorts `coll` by `f` in such a way that if partitioned by `n`, each partition will have items of similar cost.
 
 Refer to the tests for examples."
-  [{:keys [f n]
+  [{:keys [^ifn? f, ^pos-integer? n]
     :or   {f identity
            n (-> (Runtime/getRuntime) .availableProcessors)}
-    :as   options}
-   coll]
-  {:pre [(check! (partial every? #{:f :n}) (keys options)
-                 ifn? f
-                 pos-integer? n
-                 coll? coll)]}
+    :as   ^{::speced/spec (fn [o]
+                            (->> o keys (every? #{:f :n})))} options}
+   ^coll? coll]
   (if-not (seq coll)
     coll
     (->> coll
